9631550ed3ced892767aeb998ec9bb718e06a629
nikhilbad7 <nikhil.badani@gmail.com>
2026-08-13 01:56:30 +0530

    refactor product routes and controllers

---
+++ backend/controllers/productController.js
@@ -0,0 +1,22 @@
+const products = require("../data/products");
+
+const getProducts = (req, res) => {
+  res.json(products);
+};
+
+const getProductById = (req, res) => {
+  const product = products.find((p) => p.id === Number(req.params.id));
+
+  if (!product) {
+    return res.status(404).json({
+      message: "Product not found",
+    });
+  }
+
+  res.json(product);
+};
+
+module.exports = {
+  getProducts,
+  getProductById,
+};
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
-const products = require("./data/products");
-
 const express = require("express");
+
+const productRoutes = require("./routes/productRoutes");
 
 const app = express();
 
@@ -13,13 +13,7 @@ app.get("/api/health", (req, res) => {
   });
 });
 
-app.get("/api/products/:id", (req, res) => {
-  const product = products.find((p) => p.id === parseInt(req.params.id));
-  if (!product) {
-    return res.status(404).json({ message: "Product not found" });
-  }
-  res.json(product);
-});
+app.use("/api/products", productRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
